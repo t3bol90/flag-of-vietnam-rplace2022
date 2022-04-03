bca765ab807c091bed67ca587302cd9e35c4bb3c
Bùi Văn Thiện <thien.bui.84436@gmail.com>
2022-04-04 00:12:31 +0700

    Update script info; disable 50% opacity thingy

--- overlay.js
+++ overlay.js
@@ -1,29 +1,32 @@
 // ==UserScript==
-// @name         VN FLAG
+// @name         r/Place overlay for Vietnam
 // @namespace    http://tampermonkey.net/
-// @version      0.3
-// @description  try to take over the canvas!
-// @author       t3bol90, 84436
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
+// @version      alpha two
+// @description  r/Place overlay for the Vietnam flag (1490,661 -> 1561,687). Stolen from OsuPlace.
+// @author       u/oralekin (OC), u/LittleEndu, u/ekgame, u/84436, t3bol90 (not on reddit yet)
 // @match        https://hot-potato.reddit.com/embed*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @grant        none
 // ==/UserScript==
 
-// This script is forked from Osu community, but use for flag of Vietnam in r/place 2022!
 if (window.top !== window.self) {
     window.addEventListener('load', () => {
         // Load the image
         const image = document.createElement("img");
-        image.src = "https://raw.githubusercontent.com/t3bol90/flag-of-vietnam-rplace2022/main/Noomy_VN_Flag.png";
+        // image.src = "https://cdn.mirai.gg/tmp/dotted-place-template.png";
+        image.src = "https://cdn.discordapp.com/attachments/546689595409170432/960219012426530846/my.png";
         image.onload = () => {
             image.style = `position: absolute; left: 0; top: 0; width: ${image.width/3}px; height: ${image.height/3}px; image-rendering: pixelated; z-index: 1`;
         };
-
+      
         // Add the image as overlay
         const camera = document.querySelector("mona-lisa-embed").shadowRoot.querySelector("mona-lisa-camera");
         const canvas = camera.querySelector("mona-lisa-canvas");
         canvas.shadowRoot.querySelector('.container').appendChild(image);
-
+        
+        // Add a 50% overlay
+        //canvas.shadowRoot.querySelector('.container canvas').style.opacity = "0.50";
+      
         // Add a style to put a hole in the pixel preview (to see the current or desired color)
         const waitForPreview = setInterval(() => {
             const preview = camera.querySelector("mona-lisa-pixel-preview");
